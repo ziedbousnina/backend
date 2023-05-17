8b402eba938f395f05a0b5989ad74285d84d7277
Zied-bousnina <zbousnina@yahoo.com>
2023-05-17 23:18:59 +0100

    i have added some changes

--- controllers/Bin2.controller.js
+++ controllers/Bin2.controller.js
@@ -2,8 +2,52 @@ const BinModel = require("../models/Bin.model");
 
 
 
+// const CreateBin2 = async (req, res) => {
+//   console.log(req.body)
+//   try {
+//     const {
+//       name,
+//       location,
+//       address,
+//       lat,
+//       long,
+//       type,
+//       capacity,
+//       // status,
+//       gaz,
+//       niv,
+//       topicGaz,
+//       topicNiv,
+//       topicOuv
+//     } = req.body;
+
+//     // Create a new instance of Bin1 using the request body data
+//     const newBin = new BinModel({
+//       name,
+//       location,
+//       address,
+//       lat,
+//       long,
+//       type,
+//       capacity,
+//       status: false,
+//       gaz,
+//       niv,
+//       topicGaz,
+//       topicNiv,
+//       topicOuv
+//     });
+
+//     // Save the newBin instance to the database
+//     await newBin.save();
+
+//     res.status(201).json({ success: true, message: 'Bin created successfully' });
+//   } catch (error) {
+//     res.status(500).json({ success: false, error: 'Failed to create bin' });
+//   }
+// };
 const CreateBin2 = async (req, res) => {
-  console.log(req.body)
+  console.log(req.body);
   try {
     const {
       name,
@@ -13,13 +57,25 @@ const CreateBin2 = async (req, res) => {
       long,
       type,
       capacity,
-      // status,
       gaz,
       niv,
       topicGaz,
       topicNiv,
       topicOuv
     } = req.body;
+
+    // Check if the topics already exist
+    const existingBin = await BinModel.findOne({
+      $or: [
+        { topicGaz },
+        { topicNiv },
+        { topicOuv }
+      ]
+    });
+
+    if (existingBin) {
+      return res.status(400).json({ success: false, error: 'Topics must be unique' });
+    }
 
     // Create a new instance of Bin1 using the request body data
     const newBin = new BinModel({
@@ -43,7 +99,7 @@ const CreateBin2 = async (req, res) => {
 
     res.status(201).json({ success: true, message: 'Bin created successfully' });
   } catch (error) {
-    res.status(500).json({ success: false, error: 'Failed to create bin' });
+    res.status(500).json({ success: false, error: 'Topics must be unique' });
   }
 };
 
--- models/Bin.model.js
+++ models/Bin.model.js
@@ -1,29 +1,25 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+
 const binSchema1 = mongoose.Schema(
-    {
-        name: String,
-        location: String,
-        address :String,
-        lat: String,
-        long: String, 
-        type: String,
-        capacity: String,
-        status: Boolean,
-        gaz : String,
-        niv : String,
-        topicGaz :String,
-        topicNiv :String,
-        topicOuv: String
+  {
+    name: String,
+    location: String,
+    address: String,
+    lat: String,
+    long: String,
+    type: String,
+    capacity: String,
+    status: Boolean,
+    gaz: String,
+    niv: String,
+    topicGaz: { type: String, unique: true },
+    topicNiv: { type: String, unique: true },
+    topicOuv: { type: String, unique: true },
+  },
+  {
+    timestamps: true
+  }
+);
 
-
-       
-      
-
-    },{
-        timestamps: true
-    }
-)
-
-module.exports = mongoose.model('Bin1', binSchema1)
-
+module.exports = mongoose.model('Bin1', binSchema1);
