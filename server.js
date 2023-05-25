be346ca453be00dcc3947ed7e2b06e39d733491b
Zied-bousnina <zbousnina@yahoo.com>
2023-05-25 19:47:58 +0100

    I have removed the test

--- server.js
+++ server.js
@@ -67,13 +67,7 @@ app.get('/api/governorates', (req, res)=>{
   
   
 })
-app.get('/zied', (req, res)=>{
 
-  res.send('hello world')
-  
-  
-  
-})
 
 app.get('/health', function(req,res) {
   res.status(200).json({msg: 'Server is running'})
